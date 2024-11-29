388bb1f27b94b77cd60b480716453d54f941ef82
linaCodee <162879859+linaCodee@users.noreply.github.com>
2024-11-29 11:16:30 +0100

    Delete wp-aud directory

--- wp-aud/src/main/java/mk/ukim/finki/wpaud/model/exception/InvalidArgumentException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.wpaud.model.exception;
-
-public class InvalidArgumentException extends RuntimeException{
-    public InvalidArgumentException(){
-        super("Invalid arguments exception");
-    }
-}
